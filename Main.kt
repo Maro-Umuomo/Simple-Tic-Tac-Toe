6687894db405b7f1f92619437b5f06c250ec05ad
Maro Umuomo <oghenemaro.umuomo@outlook.com>
2023-09-01 16:25:17 +0200

    Implement all features for a complete multiplayer of Tic tac toe

--- Main.kt
+++ Main.kt
@@ -1,6 +1,26 @@
-package tictactoe
+fun checkWinner(input: Array<Array<String>>): MutableList<String> {
 
-fun formatStrings(input: String): String = input.replace('_', ' ')
+    val win1 = "${input[0][0]}${input[0][1]}${input[0][2]}"
+    val win2 = "${input[1][0]}${input[1][1]}${input[1][2]}"
+    val win3 = "${input[2][0]}${input[2][1]}${input[2][2]}"
+    val win4 = "${input[0][0]}${input[1][0]}${input[2][0]}"
+    val win5 = "${input[0][1]}${input[1][1]}${input[2][1]}"
+    val win6 = "${input[0][2]}${input[1][2]}${input[2][2]}"
+    val win7 = "${input[0][0]}${input[1][1]}${input[2][2]}"
+    val win8 = "${input[0][2]}${input[1][1]}${input[2][0]}"
+
+    val result = mutableListOf<String>()
+
+    if (win1 == "OOO" || win2 == "OOO" || win3 == "OOO" || win4 == "OOO" || win5 == "OOO" || win6 == "OOO" || win7 == "OOO" || win8 == "OOO")
+    {
+        result.add("O")
+    }
+    else if (win1 == "XXX" || win2 == "XXX" || win3 == "XXX" || win4 == "XXX" || win5 == "XXX" || win6 == "XXX" || win7 == "XXX" || win8 == "XXX")
+    {
+        result.add("X")
+    }
+    return  result
+}
 
 fun printScreen(inputArray: Array<Array<String>>){
     println("---------")
@@ -10,69 +30,101 @@ fun printScreen(inputArray: Array<Array<String>>){
     println("---------")
 }
 
-fun playUser(x: Int, y: Int, inputArray: Array<Array<String>>): Boolean {
+fun playUser(x: Int, y: Int, player: Int, inputArray: Array<Array<String>>): Boolean {
 
-    if(inputArray[x][y] == " ") {
-        inputArray[x][y] = "X"
-        return true
+    return if(inputArray[x][y] == " ") {
+        when (player) {
+            1 -> inputArray[x][y] = "X"
+            2 -> inputArray[x][y] = "O"
+        }
+        true
     }
-    else return false
+    else false
 }
 
-fun main() {
-    val inputArray = arrayOf(
-        arrayOf(" ", " ", " "),
-        arrayOf(" ", " ", " "),
-        arrayOf(" ", " ", " "),
-    )
+fun notFull(inputArray: Array<Array<String>>): Boolean {
+    var counts = 0
 
-    var inputString = readln()
-    inputString = formatStrings(inputString)
-
-    inputArray[0][0] = inputString[0].toString()
-    inputArray[0][1] = inputString[1].toString()
-    inputArray[0][2] = inputString[2].toString()
-    inputArray[1][0] = inputString[3].toString()
-    inputArray[1][1] = inputString[4].toString()
-    inputArray[1][2] = inputString[5].toString()
-    inputArray[2][0] = inputString[6].toString()
-    inputArray[2][1] = inputString[7].toString()
-    inputArray[2][2] = inputString[8].toString()
-
-    printScreen(inputArray)
-    var inputStr = readln()
-
-    while (true)
-    {
-        if (inputStr[0].isDigit() && inputStr[2].isDigit())
-            break
-        println("You should enter numbers!")
-        inputStr = readln()
+    for (array in inputArray) {
+        counts += array.count{it == " "}
     }
+    return counts > 0
+}
 
+fun getValidUserInput(player: Int, inputArray: Array<Array<String>>, play: (Int, Int, Int, Array<Array<String>>) -> Boolean): Boolean{
+    var inputStr: String
     var xCord: Int
     var yCord: Int
 
     while (true)
     {
-        var (x,y) = inputStr.split(" ")
+        inputStr = readln()
+        if (inputStr.length < 3) {
+            println("Invalid Entry! Enter another coordinate")
+            continue
+        }
+        else if ( !inputStr[0].isDigit()  || !inputStr[2].isDigit() ) {
+            println("You should enter numbers!")
+            continue
+        }
+
+        val (x,y) = inputStr.split(" ")
+
         if (x.toInt() in 1..3 && y.toInt() in 1..3)
         {
             xCord = x.toInt()
             yCord = y.toInt()
             --xCord
             --yCord
-            if (playUser(xCord, yCord, inputArray))
+            if (play(xCord, yCord, player, inputArray))
                 break
             else {
                 println("This cell is occupied! Choose another one!")
+                continue
             }
-
-        } else {
-            println("Coordinates should be from 1 to 3!")
         }
-        inputStr = readln()
+        else {
+            println("Coordinates should be from 1 to 3!")
+            continue
+        }
     }
+    return true
+}
 
+fun generateMessage(input: Array<Array<String>>) {
+    val results = checkWinner(input)
+
+    if (results.size == 1) {
+        if (results.contains("O")) println("O wins")
+        else if(results.contains("X")) println("X wins")
+    } else if (results.size > 1)
+        println("Impossible")
+    else if (results.size == 0) {
+        println("Draw")
+    }
+}
+
+fun getPlayer(count: Int): Int = (count % 2) + 1
+
+fun main() {
+
+    var count = 0   //Add 1 to determine if it's player 1 or 2.
+    var player: Int
+
+    val inputArray = arrayOf(
+        arrayOf(" ", " ", " "),
+        arrayOf(" ", " ", " "),
+        arrayOf(" ", " ", " "),
+    )
     printScreen(inputArray)
+
+    while (checkWinner(inputArray).size == 0 && notFull(inputArray))
+    {
+        player = getPlayer(count)
+        if (getValidUserInput(player, inputArray, ::playUser)) {
+            ++count
+        }
+        printScreen(inputArray)
+    }
+    generateMessage(inputArray)
 }
